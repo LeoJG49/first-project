11c70bb49f8836586ff306127ff3f3b6dfe71186
Leo Dev <leoneljovel49@gmail.com>
2026-03-03 19:22:22 +0100

    Add opinionsData.js to store dynamic opinions data for the Opinions component

---
+++ src/data/opinionsData.js
@@ -0,0 +1,32 @@
+// opinionsData.js
+
+//We will use .jsx files for our components and .js files for our data. 
+//This is a common convention in React projects to help differentiate between components and other JavaScript files. 
+//The .jsx extension indicates that the file contains JSX syntax, which is used for defining React components, while .js files are typically used for regular JavaScript code, such as data or utility functions.
+
+export const opinionsData = [
+    {
+        id: 1,
+        image: "2",
+        context: "Gold folder",
+        name: "Emma Watson",
+        job: "Actress and Marketing Manager",
+        opinion: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ut deserunt accusamus, nemo cupiditate eum! Minima suscipit assumenda at, soluta illo voluptatum voluptas natus exercitationem quis provident voluptate non laborum."
+    },
+    {
+        id: 2,
+        image: "3",
+        context: "An abstract background",
+        name: "John Smith",
+        job: "Software Engineer",
+        opinion: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ut deserunt accusamus, nemo cupiditate eum! Minima suscipit assumenda at, soluta illo voluptatum voluptas natus exercitationem quis provident voluptate non laborum."
+    },
+    {
+        id: 3,
+        image: "4",
+        context: "Seven orange cylinders on a black background",
+        name: "Sarah Johnson",
+        job: "Data Scientist",
+        opinion: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque ut deserunt accusamus, nemo cupiditate eum! Minima suscipit assumenda at, soluta illo voluptatum voluptas natus exercitationem quis provident voluptate non laborum."
+    }
+];
